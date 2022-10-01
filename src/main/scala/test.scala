1e808c7b4d73e8af83f35112df03f2092a049a8f
raghwendra-analytics <raghwendra.analytics@gmail.com>
2022-10-01 19:41:17 +0530

    baseCommit

--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -40,5 +40,4 @@ object test extends App{
     && cvd_state("cases")!=cvd_dest("cases") && cvd_state("deaths")!=cvd_dest("deaths"))
   println("number of rows with different values in source and destination: "+diffCnt.count())
 
-  println("commit1")
 }
